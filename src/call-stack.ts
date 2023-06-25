ea4e1207e44113a3caf60eacfb6e82617e26d17d
Sam Elliott <samde@microsoft.com>
2023-06-24 23:56:59 -0400

    prettier

--- src/call-stack.ts
+++ src/call-stack.ts
@@ -1,33 +1,42 @@
-import fs from 'fs'
-import chalk from 'chalk'
+import fs from "fs";
+import chalk from "chalk";
 
-export async function resolveCallStack (callStack: number[], symbolsFilePath: string): Promise<void> {
+export async function resolveCallStack(
+  callStack: number[],
+  symbolsFilePath: string
+): Promise<void> {
   // Load the symbols from the file
-  const symbolFileContent = fs.readFileSync(symbolsFilePath, 'utf8')
-  const symbolLines = symbolFileContent.split('\n')
+  const symbolFileContent = fs.readFileSync(symbolsFilePath, "utf8");
+  const symbolLines = symbolFileContent.split("\n");
 
-  type SymbolMap = Record<number, string>
-  const symbolMap: SymbolMap = {}
+  type SymbolMap = Record<number, string>;
+  const symbolMap: SymbolMap = {};
 
   // Parse the symbol file
-  symbolLines.forEach(line => {
-    const parts = line.split(':')
+  symbolLines.forEach((line) => {
+    const parts = line.split(":");
     if (parts.length >= 2) {
-      const symbolNumber = parseInt(parts[0]) // Store the number as an integer
-      const symbolName = parts.slice(1).join(':') // Join all remaining parts
-      symbolMap[symbolNumber] = symbolName
+      const symbolNumber = parseInt(parts[0]); // Store the number as an integer
+      const symbolName = parts.slice(1).join(":"); // Join all remaining parts
+      symbolMap[symbolNumber] = symbolName;
     }
-  })
+  });
 
   // Replace the hex numbers in the call stack with the symbol names
-  const resolvedCallStack = callStack.map(number => {
-    const readableFunctionName = symbolMap[number].replace(/\\20/g, ' ').replace(/\\28/g, '(').replace(/\\29/g, ')').replace(/\\2c/g, ',')
-    return readableFunctionName ?? `unknown function [${number}]`
-  })
+  const resolvedCallStack = callStack.map((number) => {
+    const readableFunctionName = symbolMap[number]
+      .replace(/\\20/g, " ")
+      .replace(/\\28/g, "(")
+      .replace(/\\29/g, ")")
+      .replace(/\\2c/g, ",");
+    return readableFunctionName ?? `unknown function [${number}]`;
+  });
 
-  console.log(chalk.green('Resolved Call Stack:'))
+  console.log(chalk.green("Resolved Call Stack:"));
   resolvedCallStack.forEach((fn, index) => {
     // Add color to make it easier to differentiate the function calls
-    console.log(chalk.blue(`Call #${index + 1}:`) + '\n' + chalk.yellow(fn) + '\n')
-  })
+    console.log(
+      chalk.blue(`Call #${index + 1}:`) + "\n" + chalk.yellow(fn) + "\n"
+    );
+  });
 }
--- src/cli.ts
+++ src/cli.ts
@@ -1,72 +1,71 @@
 #!/usr/bin/env node
 
-import { resolveCallStack } from './call-stack.js'
-import yargs from 'yargs/yargs'
-import { hideBin } from 'yargs/helpers'
-import { updateShell } from './update-shell.js'
-import inquirer from 'inquirer'
+import { resolveCallStack } from "./call-stack.js";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import { updateShell } from "./update-shell.js";
+import inquirer from "inquirer";
 
 interface CommandLineArgs {
-  callStack?: number[]
-  symbolFile?: string
-  addToPath?: boolean
+  callStack?: number[];
+  symbolFile?: string;
+  addToPath?: boolean;
 }
 
 const argv = yargs(hideBin(process.argv))
-  .option('symbolFile', {
-    type: 'string',
-    describe: 'Path to the symbol file'
+  .option("symbolFile", {
+    type: "string",
+    describe: "Path to the symbol file",
   })
-  .option('callStack', {
-    type: 'array',
-    describe: 'Array of call stack symbol indexes'
+  .option("callStack", {
+    type: "array",
+    describe: "Array of call stack symbol indexes",
   })
-  .option('addToPath', {
-    type: 'boolean',
-    describe: 'If passed, adds the binary to the PATH'
-  })
-  .argv as CommandLineArgs
+  .option("addToPath", {
+    type: "boolean",
+    describe: "If passed, adds the binary to the PATH",
+  }).argv as CommandLineArgs;
 
 if (argv.addToPath ?? false) {
-  updateShell()
+  updateShell();
 }
 
-async function getArgsAndResolve (): Promise<void> {
-  let symbolFile = argv.symbolFile
-  let callStack = argv.callStack
+async function getArgsAndResolve(): Promise<void> {
+  let symbolFile = argv.symbolFile;
+  let callStack = argv.callStack;
 
   if (symbolFile === undefined) {
     const answers = await inquirer.prompt([
       {
-        name: 'symbolFile',
-        type: 'input',
-        message: 'Please provide the path to the symbol file:'
-      }
-    ])
-    symbolFile = answers.symbolFile
+        name: "symbolFile",
+        type: "input",
+        message: "Please provide the path to the symbol file:",
+      },
+    ]);
+    symbolFile = answers.symbolFile;
   }
 
   if (callStack === undefined) {
     const answers = await inquirer.prompt([
       {
-        name: 'callStack',
-        type: 'input',
-        message: 'Please provide the call stack symbol indexes (comma separated):',
-        filter (value) {
-          return value.split(',').map(Number)
-        }
-      }
-    ])
-    callStack = answers.callStack
+        name: "callStack",
+        type: "input",
+        message:
+          "Please provide the call stack symbol indexes (comma separated):",
+        filter(value) {
+          return value.split(",").map(Number);
+        },
+      },
+    ]);
+    callStack = answers.callStack;
   }
 
-  if (symbolFile !== undefined && (callStack != null)) {
-    await resolveCallStack(callStack, symbolFile)
+  if (symbolFile !== undefined && callStack != null) {
+    await resolveCallStack(callStack, symbolFile);
   }
 }
 
-getArgsAndResolve()
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
+getArgsAndResolve().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
